a4926b9c29542a07c60bccb51f84c2962e2abeb4
merry <k.maria.m.24.05@gmail.com>
2023-05-20 15:41:07 +0300

    add css to Profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,36 +1,39 @@
 import PropTypes from "prop-types";
-import s from './Profile.modules.scss'
+import s from './Profile.module.scss'
 
 const Profile = ({ username, tag, location, avatar, stats: { followers, views, likes } }) => {
     return (
         <div className={s.profile}>
-            <div className={s.description}>
-                <img
-                    src={avatar}
-                    alt="User avatar"
-                    className={s.avatar}
-                />
-                <p className={s.name}>{username}</p>
-                <p className={s.tag}>{tag}</p>
-                <p className={s.location}>{location}</p>
+            <div className={s.container}>
+                <div className={s.description}>
+                    <img
+                        src={avatar}
+                        alt="User avatar"
+                        className={s.avatar}
+                    />
+                    <p className={s.name}>{username}</p>
+                    <p className={s.tag}>@{tag}</p>
+                    <p className={s.location}>{location}</p>
+                </div>
+                <ul className={s.stats}>
+                    <li>
+                        <span className={s.label}>Followers</span>
+                        <span className={s.quantity}>{followers}</span>
+                    </li>
+                    <li>
+                        <span className={s.label}>Views</span>
+                        <span className={s.quantity}>{views}</span>
+                    </li>
+                    <li>
+                        <span className={s.label}>Likes</span>
+                        <span className={s.quantity}>{likes}</span>
+                    </li>
+                </ul>
             </div>
-            <ul className={s.stats}>
-                <li>
-                    <span className={s.label}>Followers</span>
-                    <span className={s.quantity}>{followers}</span>
-                </li>
-                <li>
-                    <span className={s.label}>Views</span>
-                    <span className={s.quantity}>{views}</span>
-                </li>
-                <li>
-                    <span className={s.label}>Likes</span>
-                    <span className={s.quantity}>{likes}</span>
-                </li>
-            </ul>
         </div>
     );
 };
+
 
 Profile.propTypes = {
     username: PropTypes.string.isRequired,
